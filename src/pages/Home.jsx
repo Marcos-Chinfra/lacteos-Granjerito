bd8966cde7f57532671da48c5e2d1c7a9b66d643
MSCH13 <marcoschinchilla13@gmail.com>
2023-08-14 10:03:27 -0600

    fix images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <main className='p-5 w-screen h-5/6 flex flex-col justify-center lg:flex-row '>
                 <article className='flex flex-col mx-auto justify-center w-full lg:w-3/5 max-w-loginLogo'>
                     <figure className='w-36 lg:w-52 mx-auto'>
-                        <img src="../../public/img/logo.png" alt="" />
+                        <img src="https://scontent.fgua3-3.fna.fbcdn.net/v/t39.30808-6/277097399_105355782123734_3245400114898271789_n.jpg?_nc_cat=105&cb=99be929b-3346023f&ccb=1-7&_nc_sid=be3454&_nc_ohc=-2CqyckP_dwAX_-vwL7&_nc_ht=scontent.fgua3-3.fna&oh=00_AfClqW8-0EZBiC7q-dY9Y5vi4wsnf_R7zqzjpalSuautuQ&oe=64DFA20E" alt="" />
                     </figure>
                     <div className='text-center mx-auto'>
                         <h1 className='text-3xl lg:text-4xl font-bold text-gray-600'>
